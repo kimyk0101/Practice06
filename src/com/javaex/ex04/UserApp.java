dcacd554f9e2c60b42dfedc39948fdbc713f143a
kimyk0101 <kimyk7131@gmail.com>
2024-11-11 14:33:17 +0900

    4번 문제 완료

--- src/com/javaex/ex04/UserApp.java
+++ src/com/javaex/ex04/UserApp.java
@@ -10,15 +10,18 @@ public class UserApp {
 		
 		
 		User arr[] = new User[] {c,u,e};
-			
+		int j = 0;	
+		
 		for (int i = 0; i < arr.length; ++i) {
 			arr[i].showInfo();
 			
-			if (arr[i] == e) {
-				System.out.println("운영자의 월급은 " + e.salary*10000 + "원 입니다.");
+			if (arr[i] instanceof Employee) {	//	arr[]는 현재 User이므로 Employee의 값을 가져오기 위해서는 다운캐스팅 필요, 설계도를 User에서 Employee로
+				Employee other = (Employee)arr[i];	//	other에 다운캐스팅한 Employee 할당
+				j = other.getSalary();	//	주어.동사라고 생각하기 (other에게 salary 값 달라고 하기)
 			}
+			
 		}
-		
+		System.out.println("운영자의 월급은 " + j*10000 + "원 입니다.");
 	}
 
 }
