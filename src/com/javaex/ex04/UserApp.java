88eddb9c9dddc9d2686a5dbdc797acac15e654d1
kimyk0101 <kimyk7131@gmail.com>
2024-11-11 17:24:35 +0900

    4번 문제 설명

--- src/com/javaex/ex04/UserApp.java
+++ src/com/javaex/ex04/UserApp.java
@@ -15,8 +15,10 @@ public class UserApp {
 		for (int i = 0; i < arr.length; ++i) {
 			arr[i].showInfo();
 			
+			//	만약 배열이 돌다가 Employee 방에 들어가면 그곳에서 월급 데이터만 추출
 			if (arr[i] instanceof Employee) {	//	arr[]는 현재 User이므로 Employee의 값을 가져오기 위해서는 다운캐스팅 필요, 설계도를 User에서 Employee로
-				Employee other = (Employee)arr[i];	//	other에 다운캐스팅한 Employee 할당
+				Employee other = (Employee)arr[i];	//	다운캐스팅
+				//	다운캐스팅까지 완료했으면 이제 Employee에서 데이터를 가져올 준비가 된 것
 				j = other.getSalary();	//	주어.동사라고 생각하기 (other에게 salary 값 달라고 하기)
 			}
 			
@@ -25,3 +27,4 @@ public class UserApp {
 	}
 
 }
+
