6812ea1488030b2dcccde2c634ebe9a41eb11c51
kimyk0101 <kimyk7131@gmail.com>
2024-11-11 18:20:50 +0900

    5번 문제

--- src/com/javaex/ex05/Depart.java
+++ src/com/javaex/ex05/Depart.java
@@ -1,7 +1,25 @@
 package com.javaex.ex05;
 
-public class Depart {
+public class Depart extends Employee {
 
-	 //코드작성
+	private String department;
+	
+	public Depart(String name, int salary, String department) {
+		super(name, salary);
+		this.department = department;
+	}
+	
+	public String getDepartment() {
+		return department;
+	}
 
+	public void setDepartment(String department) {
+		this.department =  department;
+	}
+	
+	@Override
+	public void showInformation() {
+		super.showInformation();
+		System.out.println("부서:" + department);
+	}	//	private이라 데이터를 가져올 수 없음
 }
