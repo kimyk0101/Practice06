3fbe18fa74b019b02612ceb051b434dfb3cc1820
kimyk0101 <kimyk7131@gmail.com>
2024-12-02 12:35:38 +0900

    상속 문제 7번까지 완료

--- src/com/javaex/ex07/SmartPhone.java
+++ src/com/javaex/ex07/SmartPhone.java
@@ -3,15 +3,18 @@ package com.javaex.ex07;
 public class SmartPhone extends MusicPhone {
     
     public void execute(String str){
+        if ("앱".equals(str)) {
+        	playApp();
+        } else {
+        	super.execute(str);
+        }
         
-        //코드작성
         
     }
  
-    //메소드작성
-    
-    //메소드작성
-    
+    protected void playApp() {
+    	System.out.println("앱 실행");
+    }
     
     
 }
